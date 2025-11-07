8a55ec9f2bac01d53fd84105d7de0cf6fb4d7794
Gabriel <massolagabriel1@gmail.com>
2025-11-07 12:44:07 -0300

    Initial commit

---
+++ src/main/java/com/ecodata/eco_data/EcoDataApplication.java
@@ -0,0 +1,13 @@
+package com.ecodata.eco_data;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcoDataApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EcoDataApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ecodata/eco_data/EcoDataApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ecodata.eco_data;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcoDataApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
